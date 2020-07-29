66159b2f1cc0b26f844a26560bed5648527ceda8
Ibraheemn13 <ibraheemn013@gmail.com>
2020-07-29 14:24:05 +0500

    sanditory
    SANDITORY YT

---
+++ contact.php
@@ -0,0 +1,25 @@
+<<?php 
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+
+
+	$email_from = 'saadystar2015@gmail.com';
+
+	$email_subject = "New Form Submission";
+
+	$email_body = "User Name :	$name.\n"
+					"User Email : $visitor_email.\n"
+					 "User Message : $message.\n";
+
+	$to = "saadystar2015@gmail.com";
+	
+	$headers = "From: $email_from \r\n";
+
+	$headers .= "Reply-to $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("location: SAAD.html");				 
+
+ ?>
